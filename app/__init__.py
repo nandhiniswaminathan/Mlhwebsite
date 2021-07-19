bf2c7536a4dfd89d75f69721d5ed449f20f5e770
Nandhini Swaminathan <nandhini.s.nitt@gmail.com>
2021-07-19 14:35:08 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,59 @@
 import os
-import flask
-from flask import Flask, render_template
-from . import db
-from werkzeug.security import check_password_hash, generate_password_hash
-from app.db import get_db
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-def create_app():
-    app = Flask(__name__)
-    app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-    db.init_app(app)
+load_dotenv()
+app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    port=5432,
+    table=os.getenv("POSTGRES_DB"),
+)
 
-    @app.route('/')
-    def index():
-        user = {'username': 'Nandhini'}
-        return render_template('home.html', title='Home', user=user)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    @app.route('/health', methods=['GET'])
-    def health():
-        return "200"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-    @app.route('/register', methods=('GET', 'POST'))
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+# links ending in ("/") will redirect to the file within the ()
+# The same style applies to the following methods
+# This allows multiple pages to interact through links.
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/health", methods=["GET"])
+def health():
+    return "200"
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         username = request.form.get("username")
@@ -40,27 +74,31 @@ def register():
             return f"User {username} created successfully"
         else:
             return error, 418
+
+    # Return a register page
     return render_template("register.html")
 
 
-    @app.route("/login", methods=["GET", "POST"])
-    def login():
-            if request.method == "POST":
-                username = request.form.get("username")
-                password = request.form.get("password")
-                error = None
-                user = UserModel.query.filter_by(username=username).first()
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
+        user = UserModel.query.filter_by(username=username).first()
 
-            if user is None:
-                error = "Incorrect username."
-            elif not check_password_hash(user.password, password):
-                error = "Incorrect password."
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user.password, password):
+            error = "Incorrect password."
 
-            if error is None:
-                return "Login Successful", 200
-            else:
-                return error, 418
+        if error is None:
+            return "Login Successful", 200
+        else:
+            return error, 418
+
+    # Return a login page
     return render_template("login.html")
 
-    return app
-app = create_app()
+if __name__ == "__main__":
+    app.run(debug=True)
